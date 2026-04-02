edbfee5bf03ad8dfd3f3bfb472c613bb3b2668fa
CubeByte <businesscube.world@gmail.com>
2026-04-02 20:07:58 +0100

    adjusted main scene with borders and got rid of temp objects

--- Assets/Scripts/Player Interaction & Control/CharacterControl.cs
+++ Assets/Scripts/Player Interaction & Control/CharacterControl.cs
@@ -7,9 +7,14 @@ public class CharacterControl : MonoBehaviour
     [Header("Referances")]
     private CharacterController controller;
     public InputActionReference interact;
+
+    [Header("Movement")] [SerializeField] private float speed = 5;
     
-    [Header("Movement")]
-    [SerializeField] private float speed = 5;
+    [Header("Gravity")]
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float groundedGravity = -2f;
+
+    private float verticalVelocity;
     
     [Header("Input")]
     private float moveInput;
@@ -41,12 +46,24 @@ public class CharacterControl : MonoBehaviour
     
     private void Movement()
     {
-        Vector3 move = new Vector3(moveInput, 0f, turnInput);
+        bool isGrounded = controller.isGrounded;
 
-        move.y = 0;
-        
+        if (isGrounded && verticalVelocity < 0)
+        {
+            verticalVelocity = groundedGravity;
+        }
+        else
+        {
+            verticalVelocity += gravity * Time.deltaTime;
+        }
+
+        Vector3 move = new Vector3(moveInput, 0f, turnInput);
+        move = transform.TransformDirection(move); // make movement relative to player
+
         move *= speed;
-        
+
+        move.y = verticalVelocity;
+
         controller.Move(move * Time.deltaTime);
     }
 
